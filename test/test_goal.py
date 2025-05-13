f440eeae6fc75000bc9644b46b7fdb5f4aa5f404
EternalCode <eternalcode0@gmail.com>
2025-05-13 18:44:50 -0400

    fix unit tests

--- test/test_goal.py
+++ test/test_goal.py
@@ -13,19 +13,20 @@ class TestVaati(MinishCapTestBase):
         """Test some different states to verify goal requires the correct items"""
         self.collect_by_name(["Small Key (DHC)", "Big Key (DHC)"])
         self.assertEqual(self.can_reach_location("DHC B1 Big Chest"), False)
-        self.collect_by_name(["Earth Element", "Fire Element", "Water Element", "Wind Element"])
+        self.collect_by_name(["Earth Element", "Fire Element", "Water Element", "Wind Element", "Progressive Sword"])
         self.assertEqual(self.can_reach_location("DHC B1 Big Chest"), True)
         self.assertBeatable(False)
-        self.collect_by_name(["Progressive Sword", "Gust Jar", "Progressive Bow", "Cane of Pacci", "Lantern", "Bomb Bag", "Spin Attack"])
+        self.collect_by_name(["Gust Jar", "Progressive Bow", "Cane of Pacci", "Lantern", "Bomb Bag", "Spin Attack"])
         self.assertBeatable(True)
 
-class TestPedestal(MinishCapTestBase):
+class TestPedestalElements(MinishCapTestBase):
     options = {
         "goal_vaati": False,
         "shuffle_elements": True,
+        "ped_swords": 0
     }
 
-    def test_goal_pedestal(self) -> None:
+    def test_goal_ped_elements(self) -> None:
         """Test whether Pedestal is only accessible once all elements are obtained"""
         self.collect_by_name(["Earth Element", "Fire Element", "Water Element"])
         self.assertBeatable(False)
