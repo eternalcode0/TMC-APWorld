3fd11650d7aec413cc66422d0b97206c5b8d7cd0
Myth197 <59457254+Myth197@users.noreply.github.com>
2025-05-30 09:48:19 +0100

    TMC: Add missing condition back for DWS Madderpillar HP
    - The previous commit removed the incorrect condition without fixing it, this adds it back with the correction.
    
    Co-authored-by: eternalcode0 <eternalcode0@gmail.com>

--- Rules.py
+++ Rules.py
@@ -778,7 +778,8 @@ class MinishCapRules:
                                                self.logic_and([self.has_all([TMCItem.GUST_JAR, TMCItem.LANTERN]),
                                                                self.has(TMCItem.SMALL_KEY_DWS, 1)])])]),
             TMCLocation.DEEPWOOD_1F_MADDERPILLAR_HP:
-                self.logic_or([self.logic_and([self.has(TMCItem.SMALL_KEY_DWS, 2), self.has(TMCItem.LANTERN)]),
+                self.logic_or([self.logic_and([self.has(TMCItem.SMALL_KEY_DWS, 4), self.has(TMCItem.GUST_JAR)]),
+                               self.logic_and([self.has(TMCItem.SMALL_KEY_DWS, 2), self.has(TMCItem.LANTERN)]),
                                self.logic_and([self.has_all([TMCItem.GUST_JAR, TMCItem.LANTERN]),
                                                self.has(TMCItem.SMALL_KEY_DWS, 1)])]),
             TMCLocation.DEEPWOOD_B1_WEST_BIG_CHEST:
